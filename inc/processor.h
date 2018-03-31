7b6682c83822b45ecb5330ed278adf09103ff562
rmaccrimm <r.maccrimm@gmail.com>
2018-03-31 17:21:33 -0600

    finished load and stack instructions

--- inc/processor.h
+++ inc/processor.h
@@ -25,7 +25,6 @@ public:
 	void map_to_memory(u8 program[], u16 size, u16 offset);
 	
 private:
-	
 	Register8bit A;
 	Register8bit F;
 	Register8bit B;
@@ -67,13 +66,55 @@ private:
 	u8 stack_pop();
 
 	// Check if there is a carry from bit 3 to bit 4 for a + b
-	bool half_carry_add(int a, int b);
+	bool half_carry_add(u16 a, u16 b);
 
 	// Check for carry from bit 4 to 3 for a - b 
-	bool half_carry_sub(int a, int b);
+	bool half_carry_sub(u16 a, u16 b);
+
+	// Check for carry from bit 7 to 8
+	bool full_carry_add(u16 a, u16 b);
 
 	// Set zero flag if register is zero, reset half carry, subtract
 	void flag_reset(Register8bit const &reg);
+
+	// Load next byte(s) in memory to reg
+	void LD_immediate(Register8bit &reg);
+	void LD_immediate(Register16bit &reg);
+
+	// Load value in src to dest
+	void LD_register(Register8bit &dest, Register8bit const &src);
+	void LD_register(Register16bit &dest, Register16bit const &src);
+
+	// Load value in memory address src to register dest
+	void LD_address(Register8bit &dest, Register16bit const &src);
+
+	// Load value in src to memory address in dest
+	void LD_address(Register16bit const &dest, Register8bit const &src);
+
+	/* Stack operations. The stack is at address 0xfffe backwards.
+	   High is pushed then low for 16 bit.
+	*/
+	void PUSH_register(Register8bit const &reg);
+	void PUSH_register(Register16bit const &reg);
+	void POP_register(Register8bit &reg);
+	void POP_register(Register16bit &reg);
+
+
+	void ADD_register(Register8bit &dest, Register8bit const &src);
+	void ADD_register(Register16bit &dest, Register16bit const &src);	
+	void SUB_register(Register8bit &dest, Register8bit const &src);
+	void SUB_register(Register16bit &dest, Register16bit const &src);
+
+	void ADC_register(Register8bit &dest, Register8bit const &src);
+	void SBC_register(Register8bit &dest, Register8bit const &src);
+
+	void ADD_immediate(Register8bit &reg);
+	void ADD_immediate(Register16bit &reg);
+	void SUB_immediate(Register8bit &reg);
+	void SUB_immediate(Register16bit &reg);
+
+	void ADD_address(Register8bit &dest, Register16bit const &src);
+	void SUB_address(Register8bit &dest, Register16bit const &src);
 
 	/*Increment/decrement register, reset subtract flag and set half 
 	  carry flag if needed
@@ -85,25 +126,6 @@ private:
 	void DEC_register(Register16bit &reg);
 	void DEC_address(Register16bit const &reg);
 
-	void ADD_immediate(Register16bit reg);
-
-	/* Load instuctions. Immediate gets next 1 or 2 bytes from memory. Address
-	   loads data from address in src to dest. Register copies between registers
-	*/
-	void LD_immediate(Register8bit &reg);
-	void LD_immediate(Register16bit &reg);
-	void LD_register(Register8bit &dest, Register8bit const &src);	
-	void LD_address(Register8bit const &dest, Register16bit const &src);
-	void LD_address(Register16bit const &dest, Register8bit const &src);
-
-	/* Stack operations. The stack is at address 0xfffe backwards.
-	   High is pushed then low for 16 bit.
-	*/
-	void PUSH_register(Register8bit const &reg);
-	void PUSH_register(Register16bit const &reg);
-	void POP_register(Register8bit &reg);
-	void POP_register(Register16bit &reg);
-
 	// Left and right rotations
 	void RL_carry(Register8bit &reg);
 	void RL_no_carry(Register8bit &reg);
@@ -113,254 +135,71 @@ private:
 	// Set zero flag if register is zero, set half carry, subtract
 	void flag_set(Register8bit const &reg);
 
-	void opcode0x00();
-	void opcode0x01();
-	void opcode0x02();
-	void opcode0x03();
-	void opcode0x04(); // INC B
-	void opcode0x05();
-	void opcode0x06(); // LD B, n
-	void opcode0x07(); // RLCA
-	void opcode0x08(); 
-	void opcode0x09();
-	void opcode0x0a();
-	void opcode0x0b();
-	void opcode0x0c(); // INC C
-	void opcode0x0d();
-	void opcode0x0e(); // LD C, n
-	void opcode0x0f(); // RRCA
-	void opcode0x10();
-	void opcode0x11(); // LD DE, nn
-	void opcode0x12();
-	void opcode0x13();
-	void opcode0x14(); // INC D
-	void opcode0x15();
-	void opcode0x16();
-	void opcode0x17(); // RLA
-	void opcode0x18();
-	void opcode0x19();
-	void opcode0x1a(); // LD A, (DE)
-	void opcode0x1b();
-	void opcode0x1c(); // INC E
-	void opcode0x1d();
-	void opcode0x1e();
-	void opcode0x1f(); // RRA
-	void opcode0x20(); // JR NZ
-	void opcode0x21(); // LD HL, nn
-	void opcode0x22();
-	void opcode0x23();
-	void opcode0x24(); // INC H
-	void opcode0x25();
-	void opcode0x26();
-	void opcode0x27();
-	void opcode0x28();
-	void opcode0x29();
-	void opcode0x2a();
-	void opcode0x2b();
-	void opcode0x2c(); // INC L
-	void opcode0x2d();
-	void opcode0x2e();
-	void opcode0x2f();
-	void opcode0x30();
-	void opcode0x31(); // LD SP, nn
-	void opcode0x32(); // LD (HL-), A
-	void opcode0x33();
-	void opcode0x34(); // INC (HL)
-	void opcode0x35();
-	void opcode0x36();
-	void opcode0x37();
-	void opcode0x38();
-	void opcode0x39();
-	void opcode0x3a();
-	void opcode0x3b();
-	void opcode0x3c(); // INC A
-	void opcode0x3d();
-	void opcode0x3e(); // LD A, n
-	void opcode0x3f();
-	void opcode0x40();
-	void opcode0x41();
-	void opcode0x42();
-	void opcode0x43();
-	void opcode0x44();
-	void opcode0x45();
-	void opcode0x46();
-	void opcode0x47();
-	void opcode0x48();
-	void opcode0x49();
-	void opcode0x4a();
-	void opcode0x4b();
-	void opcode0x4c();
-	void opcode0x4d();
-	void opcode0x4e();
-	void opcode0x4f(); // LD C, A
-	void opcode0x50();
-	void opcode0x51();
-	void opcode0x52();
-	void opcode0x53();
-	void opcode0x54();
-	void opcode0x55();
-	void opcode0x56();
-	void opcode0x57();
-	void opcode0x58();
-	void opcode0x59();
-	void opcode0x5a();
-	void opcode0x5b();
-	void opcode0x5c();
-	void opcode0x5d();
-	void opcode0x5e();
-	void opcode0x5f();
-	void opcode0x60();
-	void opcode0x61();
-	void opcode0x62();
-	void opcode0x63();
-	void opcode0x64();
-	void opcode0x65();
-	void opcode0x66();
-	void opcode0x67();
-	void opcode0x68();
-	void opcode0x69();
-	void opcode0x6a();
-	void opcode0x6b();
-	void opcode0x6c();
-	void opcode0x6d();
-	void opcode0x6e();
-	void opcode0x6f();
-	void opcode0x70();
-	void opcode0x71();
-	void opcode0x72();
-	void opcode0x73();
-	void opcode0x74();
-	void opcode0x75();
-	void opcode0x76();
-	void opcode0x77(); // LD (HL), A
-	void opcode0x78();
-	void opcode0x79();
-	void opcode0x7a();
-	void opcode0x7b();
-	void opcode0x7c();
-	void opcode0x7d();
-	void opcode0x7e();
-	void opcode0x7f();
-	void opcode0x80();
-	void opcode0x81();
-	void opcode0x82();
-	void opcode0x83();
-	void opcode0x84();
-	void opcode0x85();
-	void opcode0x86();
-	void opcode0x87();
-	void opcode0x88();
-	void opcode0x89();
-	void opcode0x8a();
-	void opcode0x8b();
-	void opcode0x8c();
-	void opcode0x8d();
-	void opcode0x8e();
-	void opcode0x8f();
-	void opcode0x90();
-	void opcode0x91();
-	void opcode0x92();
-	void opcode0x93();
-	void opcode0x94();
-	void opcode0x95();
-	void opcode0x96();
-	void opcode0x97();
-	void opcode0x98();
-	void opcode0x99();
-	void opcode0x9a();
-	void opcode0x9b();
-	void opcode0x9c();
-	void opcode0x9d();
-	void opcode0x9e();
-	void opcode0x9f();
-	void opcode0xa0();
-	void opcode0xa1();
-	void opcode0xa2();
-	void opcode0xa3();
-	void opcode0xa4();
-	void opcode0xa5();
-	void opcode0xa6();
-	void opcode0xa7();
-	void opcode0xa8();
-	void opcode0xa9();
-	void opcode0xaa();
-	void opcode0xab();
-	void opcode0xac();
-	void opcode0xad();
-	void opcode0xae();
-	void opcode0xaf(); // XOR A, A
-	void opcode0xb0();
-	void opcode0xb1();
-	void opcode0xb2();
-	void opcode0xb3();
-	void opcode0xb4();
-	void opcode0xb5();
-	void opcode0xb6();
-	void opcode0xb7();
-	void opcode0xb8();
-	void opcode0xb9();
-	void opcode0xba();
-	void opcode0xbb();
-	void opcode0xbc();
-	void opcode0xbd();
-	void opcode0xbe();
-	void opcode0xbf();
-	void opcode0xc0();
-	void opcode0xc1(); // POP BC
-	void opcode0xc2();
-	void opcode0xc3();
-	void opcode0xc4();
-	void opcode0xc5(); // PUSH BC
-	void opcode0xc6();
-	void opcode0xc7();
-	void opcode0xc8();
-	void opcode0xc9();
-	void opcode0xca();
-	void opcode0xcb();
-	void opcode0xcc();
-	void opcode0xcd(); // CALL nn
-	void opcode0xce();
-	void opcode0xcf();
-	void opcode0xd0();
-	void opcode0xd1(); // POP DE
-	void opcode0xd2();
-	void opcode0xd4();
-	void opcode0xd5(); // PUSH DE
-	void opcode0xd6();
-	void opcode0xd7();
-	void opcode0xd8();
-	void opcode0xd9();
-	void opcode0xda();
-	void opcode0xdc();
-	void opcode0xde();
-	void opcode0xdf();
-	void opcode0xe0(); // LD ($FF00 + n), A
-	void opcode0xe1(); // POP HL
-	void opcode0xe2(); // LD ($FF00 + C), A
-	void opcode0xe5(); // PUSH HL
-	void opcode0xe6();
-	void opcode0xe7();
-	void opcode0xe8();
-	void opcode0xe9();
-	void opcode0xea();
-	void opcode0xee();
-	void opcode0xef();
-	void opcode0xf0();
-	void opcode0xf1(); // POP AF
-	void opcode0xf2();
-	void opcode0xf3();
-	void opcode0xf5(); // PUSH AF
-	void opcode0xf6();
-	void opcode0xf7();
-	void opcode0xf8();
-	void opcode0xf9();
-	void opcode0xfa();
-	void opcode0xfb();
-	void opcode0xfe();
-	void opcode0xff();
+	void opcode0x00();	void opcode0x01();	void opcode0x02();	void opcode0x03();
+	void opcode0x04(); 	void opcode0x05();	void opcode0x06(); 	void opcode0x07(); 
+	void opcode0x08(); 	void opcode0x09();	void opcode0x0a();	void opcode0x0b();
+	void opcode0x0c(); 	void opcode0x0d();	void opcode0x0e(); 	void opcode0x0f();
+	void opcode0x10();	void opcode0x11(); 	void opcode0x12();	void opcode0x13();
+	void opcode0x14(); 	void opcode0x15();	void opcode0x16();	void opcode0x17(); 
+	void opcode0x18();	void opcode0x19();	void opcode0x1a(); 	void opcode0x1b();
+	void opcode0x1c(); 	void opcode0x1d();	void opcode0x1e();	void opcode0x1f(); 
+	void opcode0x20(); 	void opcode0x21(); 	void opcode0x22();	void opcode0x23();
+	void opcode0x24(); 	void opcode0x25();	void opcode0x26();	void opcode0x27();
+	void opcode0x28();	void opcode0x29();	void opcode0x2a();	void opcode0x2b();
+	void opcode0x2c(); 	void opcode0x2d();	void opcode0x2e();	void opcode0x2f();
+	void opcode0x30();	void opcode0x31(); 	void opcode0x32(); 	void opcode0x33();
+	void opcode0x34(); 	void opcode0x35();	void opcode0x36();	void opcode0x37();
+	void opcode0x38();	void opcode0x39();	void opcode0x3a();	void opcode0x3b();
+	void opcode0x3c(); 	void opcode0x3d();	void opcode0x3e(); 	void opcode0x3f();
+	void opcode0x40();	void opcode0x41();	void opcode0x42();	void opcode0x43();
+	void opcode0x44();	void opcode0x45();	void opcode0x46();	void opcode0x47();
+	void opcode0x48();	void opcode0x49();	void opcode0x4a();	void opcode0x4b();
+	void opcode0x4c();	void opcode0x4d();	void opcode0x4e();	void opcode0x4f(); 
+	void opcode0x50();	void opcode0x51();	void opcode0x52();	void opcode0x53();
+	void opcode0x54();	void opcode0x55();	void opcode0x56();	void opcode0x57();
+	void opcode0x58();	void opcode0x59();	void opcode0x5a();	void opcode0x5b();
+	void opcode0x5c();	void opcode0x5d();	void opcode0x5e();	void opcode0x5f();
+	void opcode0x60();	void opcode0x61();	void opcode0x62();	void opcode0x63();
+	void opcode0x64();	void opcode0x65();	void opcode0x66();	void opcode0x67();
+	void opcode0x68();	void opcode0x69();	void opcode0x6a();	void opcode0x6b();
+	void opcode0x6c();	void opcode0x6d();	void opcode0x6e();	void opcode0x6f();
+	void opcode0x70();	void opcode0x71();	void opcode0x72();	void opcode0x73();
+	void opcode0x74();	void opcode0x75();	void opcode0x76();	void opcode0x77(); 
+	void opcode0x78();	void opcode0x79();	void opcode0x7a();	void opcode0x7b();
+	void opcode0x7c();	void opcode0x7d();	void opcode0x7e();	void opcode0x7f();
+	void opcode0x80();	void opcode0x81();	void opcode0x82();	void opcode0x83();
+	void opcode0x84();	void opcode0x85();	void opcode0x86();	void opcode0x87();
+	void opcode0x88();	void opcode0x89();	void opcode0x8a();	void opcode0x8b();
+	void opcode0x8c();	void opcode0x8d();	void opcode0x8e();	void opcode0x8f();
+	void opcode0x90();	void opcode0x91();	void opcode0x92();	void opcode0x93();
+	void opcode0x94();	void opcode0x95();	void opcode0x96();	void opcode0x97();
+	void opcode0x98();	void opcode0x99();	void opcode0x9a();	void opcode0x9b();
+	void opcode0x9c();	void opcode0x9d();	void opcode0x9e();	void opcode0x9f();
+	void opcode0xa0();	void opcode0xa1();	void opcode0xa2();	void opcode0xa3();
+	void opcode0xa4();	void opcode0xa5();	void opcode0xa6();	void opcode0xa7();
+	void opcode0xa8();	void opcode0xa9();	void opcode0xaa();	void opcode0xab();
+	void opcode0xac();	void opcode0xad();	void opcode0xae();	void opcode0xaf(); 
+	void opcode0xb0();	void opcode0xb1();	void opcode0xb2();	void opcode0xb3();
+	void opcode0xb4();	void opcode0xb5();	void opcode0xb6();	void opcode0xb7();
+	void opcode0xb8();	void opcode0xb9();	void opcode0xba();	void opcode0xbb();
+	void opcode0xbc();	void opcode0xbd();	void opcode0xbe();	void opcode0xbf();
+	void opcode0xc0();	void opcode0xc1(); 	void opcode0xc2();	void opcode0xc3();
+	void opcode0xc4();	void opcode0xc5(); 	void opcode0xc6();	void opcode0xc7();
+	void opcode0xc8();	void opcode0xc9();	void opcode0xca();	void opcode0xcb();
+	void opcode0xcc();	void opcode0xcd(); 	void opcode0xce();	void opcode0xcf();
+	void opcode0xd0();	void opcode0xd1(); 	void opcode0xd2();	void opcode0xd4();
+	void opcode0xd5(); 	void opcode0xd6();	void opcode0xd7();	void opcode0xd8();
+	void opcode0xd9();	void opcode0xda();	void opcode0xdc();	void opcode0xde();
+	void opcode0xdf();	void opcode0xe0(); 	void opcode0xe1(); 	void opcode0xe2(); 
+	void opcode0xe5(); 	void opcode0xe6();	void opcode0xe7();	void opcode0xe8();
+	void opcode0xe9();	void opcode0xea();	void opcode0xee();	void opcode0xef();
+	void opcode0xf0();	void opcode0xf1(); 	void opcode0xf2();	void opcode0xf3();
+	void opcode0xf5(); 	void opcode0xf6();	void opcode0xf7();	void opcode0xf8();
+	void opcode0xf9();	void opcode0xfa();	void opcode0xfb();	void opcode0xfe();
+	void opcode0xff();	
 	
-	void cb_opcode0x11(); // RL C
-	void cb_opcode0x7c(); // BIT 7, H
+	void cb_opcode0x11(); 
+	void cb_opcode0x7c(); 
 
 };
 
--- src/opcodes.cpp
+++ src/opcodes.cpp
@@ -30,18 +30,80 @@ void Processor::stack_push(u8 data)
 u8 Processor::stack_pop()
 {
 	u8 data = memory[SP.value()];
-	SP.decrement();
+	SP.increment();
 	return data;
 }
 
-bool Processor::half_carry_add(int a, int b)
+bool Processor::half_carry_add(u16 a, u16 b)
 {
 	return (((a & 0xf) + (b & 0xf)) & 0x10) == 0x10;
 }
 
-bool Processor::half_carry_sub(int a, int b)
+bool Processor::half_carry_sub(u16 a, u16 b)
 {
 	return ((a - (b & 0xff)) & 0x10) == 0;
+}
+
+bool Processor::full_carry_add(u16 a, u16 b)
+{
+	return (((a & 0xff) + (b & 0xff)) & 0x100) == 0x100;
+}
+
+
+
+void Processor::LD_immediate(Register8bit &reg)
+{
+	reg.set(fetch_byte());
+}
+
+void Processor::LD_immediate(Register16bit &reg)
+{
+	u8 l = fetch_byte(); 
+	u8 h = fetch_byte();
+	reg.set_low(l);
+	reg.set_high(h);
+}
+
+void Processor::LD_address(Register8bit &dest, Register16bit const &src)
+{
+	dest.set(memory[src.value()]);
+}
+
+void Processor::LD_address(Register16bit const &dest, Register8bit const &src)
+{
+	memory[dest.value()] = src.value();
+}
+
+void Processor::LD_register(Register8bit &dest, Register8bit const &src)
+{
+	dest.set(src.value());
+}
+
+void Processor::LD_register(Register16bit &dest, Register16bit const &src)
+{
+	dest.set(src.value());
+}
+
+void Processor::PUSH_register(Register8bit const &reg)
+{
+	stack_push(reg.value());
+}
+
+void Processor::PUSH_register(Register16bit const &reg)
+{
+	stack_push(reg.value_high());
+	stack_push(reg.value_low());
+}
+
+void Processor::POP_register(Register8bit &reg)
+{
+	reg.set(stack_pop());
+}
+
+void Processor::POP_register(Register16bit &reg)
+{
+	reg.set_low(stack_pop());
+	reg.set_high(stack_pop());
 }
 
 void Processor::INC_register(Register8bit &reg) 
@@ -76,54 +138,72 @@ void Processor::DEC_address(Register16bit const &reg)
 	zero_flag = (memory[reg.value()] == 0);
 }
 
-void Processor::LD_immediate(Register8bit &reg)
+void Processor::ADD_register(Register8bit &dest, Register8bit const &src)
 {
-	reg.set(fetch_byte());
+	half_carry_flag = half_carry_add(dest.value(), src.value());
+	carry_flag = full_carry_add(dest.value(), src.value());
+	subtract_flag.reset();
+	dest.set(dest.value() + src.value());
+	zero_flag = (dest.value() == 0);
 }
 
-void Processor::LD_immediate(Register16bit &reg)
+void Processor::ADD_register(Register16bit &dest, Register16bit const &src)
 {
-	u8 l = fetch_byte(); 
-	u8 h = fetch_byte();
-	reg.set_low(l);
-	reg.set_high(h);
+	half_carry_flag = half_carry_add(dest.value(), src.value());
+	carry_flag = full_carry_add(dest.value(), src.value());
+	subtract_flag.reset();
+	dest.set(dest.value() + src.value());
+	zero_flag = (dest.value() == 0);
 }
 
-void Processor::LD_address(Register8bit &dest, Register16bit const &src)
+void SUB_register(Register8bit &dest, Register8bit const &src)
 {
-	dest.set(memory[src.value()]);
+
 }
 
-void Processor::LD_address(Register16bit const &dest, Register8bit const &src)
+void SUB_register(Register16bit &dest, Register16bit const &src)
 {
-	memory[dest.value()] = src.value();
+
 }
 
-void Processor::LD_register(Register8bit &dest, Register8bit const &src)
+void ADC_register(Register8bit &dest, Register8bit const &src)
 {
-	dest.set(src.value());
+
 }
 
-void Processor::PUSH_register(Register8bit const &reg)
+void SBC_register(Register8bit &dest, Register8bit const &src)
 {
-	stack_push(reg.value());
+
 }
 
-void Processor::PUSH_register(Register16bit const &reg)
+void ADD_immediate(Register8bit &reg)
 {
-	stack_push(reg.value_high());
-	stack_push(reg.value_low());
+
 }
 
-void Processor::POP_register(Register8bit &reg)
+void ADD_immediate(Register16bit &reg)
 {
-	reg.set(stack_pop());
+
 }
 
-void Processor::POP_register(Register16bit &reg)
+void SUB_immediate(Register8bit &reg)
 {
-	reg.set_low(stack_pop());
-	reg.set_high(stack_pop());
+
+}
+
+void SUB_immediate(Register16bit &reg)
+{
+
+}
+
+void ADD_address(Register8bit &dest, Register16bit const &src)
+{
+
+}
+
+void SUB_address(Register8bit &dest, Register16bit const &src)
+{
+
 }
 
 void Processor::RL_carry(Register8bit &reg)
@@ -166,8 +246,6 @@ void Processor::flag_reset(Register8bit const &reg)
 	half_carry_flag.reset();	
 }
 
-void Processor::opcode0x05() {}
-
 void Processor::opcode0x3c() { INC_register(A); }
 void Processor::opcode0x04() { INC_register(B); }
 void Processor::opcode0x0c() { INC_register(C); }
@@ -177,22 +255,16 @@ void Processor::opcode0x24() { INC_register(H); }
 void Processor::opcode0x2c() { INC_register(L); }
 void Processor::opcode0x34() { INC_address(HL); }
 
+/* 
+ *	8-bit Loads
+ */
+void Processor::opcode0x3e() { LD_immediate(A); }
 void Processor::opcode0x06() { LD_immediate(B); }
 void Processor::opcode0x0e() { LD_immediate(C); }
 void Processor::opcode0x16() { LD_immediate(D); }
 void Processor::opcode0x1e() { LD_immediate(E); }
 void Processor::opcode0x26() { LD_immediate(H); }
 void Processor::opcode0x2e() { LD_immediate(L); }
-void Processor::opcode0x3e() { LD_immediate(A); }
-
-void Processor::opcode0x7f() { LD_register(A, A); }
-void Processor::opcode0x78() { LD_register(A, B); }
-void Processor::opcode0x79() { LD_register(A, C); }
-void Processor::opcode0x7a() { LD_register(A, D); }
-void Processor::opcode0x7b() { LD_register(A, E); }
-void Processor::opcode0x7c() { LD_register(A, H); }
-void Processor::opcode0x7d() { LD_register(A, L); }
-void Processor::opcode0x7e() { LD_address(A, HL); }
 
 void Processor::opcode0x40() { LD_register(B, B); }
 void Processor::opcode0x41() { LD_register(B, C); }
@@ -201,6 +273,7 @@ void Processor::opcode0x43() { LD_register(B, E); }
 void Processor::opcode0x44() { LD_register(B, H); }
 void Processor::opcode0x45() { LD_register(B, L); }
 void Processor::opcode0x46() { LD_address(B, HL); }
+void Processor::opcode0x47() { LD_register(B, A); }
 
 void Processor::opcode0x48() { LD_register(C, B); }
 void Processor::opcode0x49() { LD_register(C, C); }
@@ -209,6 +282,7 @@ void Processor::opcode0x4b() { LD_register(C, E); }
 void Processor::opcode0x4c() { LD_register(C, H); }
 void Processor::opcode0x4d() { LD_register(C, L); }
 void Processor::opcode0x4e() { LD_address(C, HL); }
+void Processor::opcode0x4f() { LD_register(C, A); }
 
 void Processor::opcode0x50() { LD_register(D, B); }
 void Processor::opcode0x51() { LD_register(D, C); }
@@ -217,6 +291,7 @@ void Processor::opcode0x53() { LD_register(D, E); }
 void Processor::opcode0x54() { LD_register(D, H); }
 void Processor::opcode0x55() { LD_register(D, L); }
 void Processor::opcode0x56() { LD_address(D, HL); }
+void Processor::opcode0x57() { LD_register(D, A); }
 
 void Processor::opcode0x58() { LD_register(E, B); }
 void Processor::opcode0x59() { LD_register(E, C); }
@@ -225,6 +300,7 @@ void Processor::opcode0x5b() { LD_register(E, E); }
 void Processor::opcode0x5c() { LD_register(E, H); }
 void Processor::opcode0x5d() { LD_register(E, L); }
 void Processor::opcode0x5e() { LD_address(E, HL); }
+void Processor::opcode0x5f() { LD_register(E, A); }
 
 void Processor::opcode0x60() { LD_register(H, B); }
 void Processor::opcode0x61() { LD_register(H, C); }
@@ -233,6 +309,7 @@ void Processor::opcode0x63() { LD_register(H, E); }
 void Processor::opcode0x64() { LD_register(H, H); }
 void Processor::opcode0x65() { LD_register(H, L); }
 void Processor::opcode0x66() { LD_address(H, HL); }
+void Processor::opcode0x67() { LD_register(H, A); }
 
 void Processor::opcode0x68() { LD_register(L, B); }
 void Processor::opcode0x69() { LD_register(L, C); }
@@ -241,6 +318,7 @@ void Processor::opcode0x6b() { LD_register(L, E); }
 void Processor::opcode0x6c() { LD_register(L, H); }
 void Processor::opcode0x6d() { LD_register(L, L); }
 void Processor::opcode0x6e() { LD_address(L, HL); }
+void Processor::opcode0x6f() { LD_register(L, A); }
 
 void Processor::opcode0x70() { LD_address(HL, B); }
 void Processor::opcode0x71() { LD_address(HL, C); }
@@ -250,6 +328,89 @@ void Processor::opcode0x74() { LD_address(HL, H); }
 void Processor::opcode0x75() { LD_address(HL, L); }
 void Processor::opcode0x36() { memory[HL.value()] = fetch_byte(); }
 
+void Processor::opcode0x78() { LD_register(A, B); }
+void Processor::opcode0x79() { LD_register(A, C); }
+void Processor::opcode0x7a() { LD_register(A, D); }
+void Processor::opcode0x7b() { LD_register(A, E); }
+void Processor::opcode0x7c() { LD_register(A, H); }
+void Processor::opcode0x7d() { LD_register(A, L); }
+void Processor::opcode0x7f() { LD_register(A, A); }
+
+void Processor::opcode0x0a() { LD_address(A, BC); }
+void Processor::opcode0x1a() { LD_address(A, DE); }
+void Processor::opcode0x7e() { LD_address(A, HL); }
+void Processor::opcode0xfa() 
+{ 
+	u8 l = fetch_byte(); 
+	u8 h = fetch_byte();
+	A.set(memory[h << 8 | l]);
+}
+void Processor::opcode0x02() { LD_address(BC, A); }
+void Processor::opcode0x12() { LD_address(DE, A); }
+void Processor::opcode0x77() { LD_address(HL, A); }
+void Processor::opcode0xea() 
+{ 
+	u8 l = fetch_byte(); 
+	u8 h = fetch_byte();
+	memory[h << 8 | l] = A.value();
+}
+
+void Processor::opcode0xf2() { A.set(memory[0xff00 + C.value()]); }
+void Processor::opcode0xe2() { memory[0xff00 + C.value()] = A.value(); }
+
+void Processor::opcode0x3a() { LD_address(A, HL); HL.decrement(); }
+void Processor::opcode0x32() { LD_address(HL, A); HL.decrement(); }
+void Processor::opcode0x2a() { LD_address(A, HL); HL.increment(); }
+void Processor::opcode0x22() { LD_address(HL, A); HL.increment(); }
+
+void Processor::opcode0xe0() { memory[0xff00 + fetch_byte()] = A.value(); }
+void Processor::opcode0xf0() { A.set(memory[0xff00 + fetch_byte()]); }
+
+/*
+ *	16-bit loads
+ */
+void Processor::opcode0x01() { LD_immediate(BC); }
+void Processor::opcode0x11() { LD_immediate(DE); }
+void Processor::opcode0x21() { LD_immediate(HL); }
+void Processor::opcode0x31() { LD_immediate(SP); }
+
+void Processor::opcode0xf9() { LD_register(SP, HL); }
+
+void Processor::opcode0xf8()
+{
+	u8 n = fetch_byte();
+	HL.set(SP.value() + n);
+	zero_flag.reset();
+	subtract_flag.reset();
+	half_carry_flag = half_carry_add(SP.value(), n);
+	carry_flag = full_carry_add(SP.value(), n);
+} 
+
+void Processor::opcode0x08()
+{
+	u8 l = fetch_byte();
+	u8 h = fetch_byte();
+	u16 addr = h << 8 | l;
+	memory[addr] = SP.value_low();
+	memory[addr + 1] = SP.value_high();
+}
+
+void Processor::opcode0xf5() { PUSH_register(AF); }
+void Processor::opcode0xc5() { PUSH_register(BC); }
+void Processor::opcode0xd5() { PUSH_register(DE); }
+void Processor::opcode0xe5() { PUSH_register(HL); }
+
+void Processor::opcode0xf1() { POP_register(AF); }
+void Processor::opcode0xc1() { POP_register(BC); }
+void Processor::opcode0xd1() { POP_register(DE); }
+void Processor::opcode0xe1() { POP_register(HL); }
+
+/*
+ *	8-bit ALU
+ */
+
+
+
 void Processor::opcode0x20()
 {
 	i8 jump = fetch_byte();
@@ -258,26 +419,10 @@ void Processor::opcode0x20()
 	}
 }
 
-void Processor::opcode0x1a() { LD_address(A, DE); }
-
-void Processor::opcode0x11() { LD_immediate(DE); }
-void Processor::opcode0x21() { LD_immediate(HL); }
-void Processor::opcode0x31() { LD_immediate(SP); }
-
 void Processor::opcode0x07() { RL_no_carry(A); }
 void Processor::opcode0x0f() { RR_no_carry(A); }
 void Processor::opcode0x17() { RL_carry(A); }
 void Processor::opcode0x1f() { RR_carry(A); }
-
-void Processor::opcode0x32()
-{
-	memory[HL.value()] = A.value();
-	HL.decrement();
-}
-
-void Processor::opcode0x4f() { LD_register(C, A); }
-
-void Processor::opcode0x77() { memory[HL.value()] = A.value(); }
 
 void Processor::opcode0xaf()
 {
@@ -288,15 +433,9 @@ void Processor::opcode0xaf()
 	carry_flag.reset();
 }
 
-void Processor::opcode0xf1() { POP_register(AF); }
-void Processor::opcode0xc1() { POP_register(BC); }
-void Processor::opcode0xd1() { POP_register(DE); }
-void Processor::opcode0xe1() { POP_register(HL); }
 
-void Processor::opcode0xf5() { PUSH_register(AF); }
-void Processor::opcode0xc5() { PUSH_register(BC); }
-void Processor::opcode0xd5() { PUSH_register(DE); }
-void Processor::opcode0xe5() { PUSH_register(HL); }
+
+
 
 
 
@@ -306,8 +445,7 @@ void Processor::opcode0xcd()
 	LD_immediate(PC);
 }
 
-void Processor::opcode0xe0() { memory[0xff00 + fetch_byte()] = A.value(); }
-void Processor::opcode0xe2() { memory[0xff00 + C.value()] = A.value(); }
+
 
 void Processor::cb_opcode0x11() { RL_carry(C); }
 
