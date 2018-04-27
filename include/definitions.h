68e32db4680968b4a45c08bf2ddc04ea9d685ce3
Roddy MacCrimmon <rmaccrimm@gmail.com>
2018-04-27 14:32:30 -0600

    renamed inc to include

---
+++ include/definitions.h
@@ -0,0 +1,8 @@
+#include <cstdint>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+
---
+++ include/functions.h
@@ -0,0 +1,21 @@
+#include "definitions.h"
+#include <vector>
+
+std::vector<u8> read_rom(size_t &file_size, std::string path);
+
+void set(bool &b);
+
+void reset(bool &b);
+
+// Check for carry from bit 3 to bit 4 in a + b
+bool half_carry_add(u16 a, u16 b);
+
+// Check for borrow from bit 4 to 3 in a - b 
+bool half_carry_sub(u16 a, u16 b);
+
+// Check for carry from bit 7 to bit 8 in a + b
+bool full_carry_add(u16 a, u16 b);
+
+// Check for borrow from bit 8 to 7 in a - b 
+bool full_carry_sub(u16 a, u16 b);
+
