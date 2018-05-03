f81f858b59873656f808931c2d9898aeb549a8d0
Roddy MacCrimmon <rmaccrimm@gmail.com>
2018-05-03 15:06:03 -0600

    Draw generated texture to window

--- src/main.cpp
+++ src/main.cpp
@@ -17,13 +17,14 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-
-
-const float screen_quad[] = {  
-    -1.0f,  1.0f, 0.0f, 1.0f,
-    -1.0f, -1.0f, 0.0f, 0.0f,
-     1.0f, -1.0f, 1.0f, 0.0f,
-     1.0f,  1.0f, 1.0f, 1.0f
+float screen_quad[] = {  
+//  position       texture coords      
+    -1,  1,  0,    0, 1,
+    -1, -1,  0,    0, 0,
+     1, -1,  0,    1, 0,
+    -1,  1,  0,    0, 1,
+     1, -1,  0,    1, 0,
+     1,  1,  0,    1, 1
 };
 
 void compile_shader(GLuint &shader_id)
@@ -31,7 +32,7 @@ void compile_shader(GLuint &shader_id)
     const std::string vertex_src = 
         "#version 330 core\n"
         "layout (location = 0) in vec3 inPos;\n"
-        "layout (location = 2) in vec2 inTexCoords;\n"
+        "layout (location = 1) in vec2 inTexCoords;\n"
         "out vec2 texCoords;\n"
         "void main() {\n"
             "gl_Position = vec4(inPos, 1.0);\n"
@@ -55,7 +56,7 @@ void compile_shader(GLuint &shader_id)
     glCompileShader(vertex);
     glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
     if (!success) {
-        glGetShaderInfoLog(vertex, 512, NULL, err_log);
+        glGetShaderInfoLog(vertex, 512, nullptr, err_log);
         std::cout << "Vertex shader compilation failed: " << err_log << std::endl;
     }
 
@@ -64,7 +65,7 @@ void compile_shader(GLuint &shader_id)
     glCompileShader(fragment);
     glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
     if (!success) {
-        glGetShaderInfoLog(fragment, 512, NULL, err_log);
+        glGetShaderInfoLog(fragment, 512, nullptr, err_log);
         std::cout << "Fragment shader compilation failed: " << err_log << std::endl;
     }
 
@@ -121,21 +122,62 @@ int main(int argc, char *argv[])
 
     GLuint shader;
     compile_shader(shader);
+    // Set texture uniform in shader
+    glUseProgram(shader);
+    int uniformloc = glGetUniformLocation(shader, "screen_texture");
+    if (uniformloc == -1) {
+        std::cout << "Error: Uniform \"screen_texture\" not found" << std::endl;
+    }
+    glUniform1i(uniformloc, 0);
 
+    // Create new texture object
     GLuint screen_tex;
     glGenTextures(1, &screen_tex);
     glBindTexture(GL_TEXTURE_2D, screen_tex);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_FLOAT, pixels);
+	// No texture smoothing
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    // Set active texture unit
+    glActiveTexture(GL_TEXTURE0);
+    
+    GLuint screen_vao;
+    GLuint screen_vbo;
+    glGenVertexArrays(1, &screen_vao);
+    glGenBuffers(1, &screen_vbo);
+    glBindVertexArray(screen_vao);
+    glBindBuffer(GL_ARRAY_BUFFER, screen_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(screen_quad), screen_quad, GL_STATIC_DRAW);
+    // vertex positions
+    glEnableVertexAttribArray(0);    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    // vertex texture coordinates
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    //glBindVertexArray(0);
 
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+	glDisable(GL_DEPTH_TEST);
+	
 	SDL_Event event;
     while (true) {
         SDL_PollEvent(&event);
         if (event.type == SDL_QUIT) {
             break;
         }
-        glUseProgram(shader);
+		glClearColor(0.0, 0.0, 0.0, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// set texture data
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_FLOAT, pixels);
+
+        glDrawArrays(GL_TRIANGLES, 0, 6); 
         SDL_GL_SwapWindow(window);
     }
+
+	glBindVertexArray(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
 
     SDL_GL_DeleteContext(glcontext);
     SDL_Quit();
