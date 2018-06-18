e8f6f28fe29e441cd2963dae966177898d158b77
Roddy MacCrimmon <rmaccrimm@gmail.com>
2018-06-17 22:06:34 -0600

    Rename memory files to mmu to avoid conflict with std lib

---
+++ include/mmu.h
@@ -0,0 +1,20 @@
+#ifndef MMU_H
+#define MMU_H
+
+#include "definitions.h"
+
+class Memory
+{
+public:
+    Memory();
+    void write(u16 addr, u8 data);
+    u8 read(u16 addr) const;
+    void set_flags(u16 addr, u8 mask, bool b);
+    void load_cart(const char *file_path, size_t offset = 0x100);
+    void load(u8 data[], size_t offset, size_t size);
+
+private:
+    u8 mem[0x10000];
+};
+
+#endif
