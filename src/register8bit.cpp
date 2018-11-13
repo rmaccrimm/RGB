cf8108c76378eb21064a97a6a801c98c8a8d664f
rmaccrimmon <r.maccrimm@gmail.com>
2018-11-13 12:45:11 -0700

    Add write method allowing 8bit registers to have read-only bits

--- src/register8bit.cpp
+++ src/register8bit.cpp
@@ -1,6 +1,8 @@
 #include "register8bit.h"
 
-Register8bit::Register8bit(): byte(0) {}
+Register8bit::Register8bit(): byte(0), write_mask(0xff) {}
+
+Register8bit::Register8bit(u8 w_mask): byte(0), write_mask(w_mask) {}
 
 u8 Register8bit::value() const { return byte; }
 
@@ -13,3 +15,8 @@ void Register8bit::decrement() { byte--; }
 void Register8bit::add(u8 x) { byte += x; }
 
 void Register8bit::sub(u8 x) { byte -= x; }
+
+void Register8bit::write(u8 x) 
+{ 
+    byte = (x & write_mask) | (byte & ~write_mask); 
+}
