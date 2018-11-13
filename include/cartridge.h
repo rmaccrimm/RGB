2f00f67e1434040d7811c330330789eb6a4d832d
rmaccrimmon <r.maccrimm@gmail.com>
2018-11-12 19:07:12 -0700

    Load rom file in constructor

--- include/cartridge.h
+++ include/cartridge.h
@@ -8,62 +8,65 @@
 class Cartridge
 {
 public:
-    enum CartridgeType {
-        ROM_ONLY                        = 0x00,
-        MBC1                            = 0x01,
-        MBC1_RAM                        = 0x02,
-        MBC1_RAM_BATTERY                = 0x03,
-        MBC2                            = 0x05,
-        MBC2_BATTERY                    = 0x06,
-        ROM_RAM                         = 0x08,
-        ROM_RAM_BATTERY                 = 0x09,
-        MMM01                           = 0x0b,
-        MMM01_RAM                       = 0x0c,
-        MMM01_RAM_BATTERY               = 0x0d,
-        MBC3_TIMER_BATTERY              = 0x0f,
-        MBC3_RAM_TIMER_BATTERY          = 0x10,
-        MBC3                            = 0x11,
-        MBC3_RAM                        = 0x12,
-        MBC3_RAM_BATTERY                = 0x13,
-        MBC5                            = 0x19,
-        MBC5_RAM                        = 0x1a,
-        MBC5_RAM_BATTERY                = 0x1b,
-        MBC5_RUMBLE                     = 0x1c,
-        MBC5_RUMBLE_RAM                 = 0x1d,
-        MBC5_RUMBLE_RAM_BATTERY         = 0x1e,
-        MBC6                            = 0x20,
-        MBC7_SENSOR_RUMBLE_RAM_BATTERY  = 0x22,
-        POCKET_CAMERA                   = 0xfc,
-        BANDAI_TAMA5                    = 0xfd,
-        HuC3                            = 0xfe,
-        HuC1_RAM_BATTERY                = 0xff
-    };
-
-    enum Mode { ROM, RAM };
-
     Cartridge(std::string filename); 
 
     u8 read(u16 addr);
     void write(u16 addr, u8 val);
+    std::string type();
+    std::string title();
+
+    enum Mode { ROM, RAM };
+
+    enum CartridgeType {
+        INVALID,                
+        ROM_ONLY,                        
+        MBC1,                            
+        MBC1_RAM,                        
+        MBC1_RAM_BATTERY,                
+        MBC2,                            
+        MBC2_BATTERY,                    
+        ROM_RAM,                         
+        ROM_RAM_BATTERY,                 
+        MMM01,                           
+        MMM01_RAM,                       
+        MMM01_RAM_BATTERY,               
+        MBC3_TIMER_BATTERY,              
+        MBC3_RAM_TIMER_BATTERY,          
+        MBC3,                            
+        MBC3_RAM,                        
+        MBC3_RAM_BATTERY,                
+        MBC5,                            
+        MBC5_RAM,                        
+        MBC5_RAM_BATTERY,                
+        MBC5_RUMBLE,                     
+        MBC5_RUMBLE_RAM,                 
+        MBC5_RUMBLE_RAM_BATTERY,         
+        MBC6,                            
+        MBC7_SENSOR_RUMBLE_RAM_BATTERY,  
+        POCKET_CAMERA,                   
+        BANDAI_TAMA5,                    
+        HuC3,                            
+        HuC1_RAM_BATTERY
+    };
 
 private:
     CartridgeType cartridge_type;
-    std::string title;
+    std::string game_title;
     bool mode;
     bool gbc; // color cartridge
     bool sgb; // super gameboy functions
     bool enable_ram;
+    int cartridge_file_size;
     int rom_size;
     int ram_size;
     const int ram_bank_size;
     const int rom_bank_size;
-    
+
     u8 current_rom_bank;
     u8 current_ram_bank;
-    std::vector<u8> rom_data;
+    std::vector<u8> cartridge_data;
 
-    std::vector<u8> read_rom(size_t &Filesize, std::string path);
-
+    CartridgeType get_type(u8 val);
 };
 
 #endif
---
+++ src/cartridge.cpp
@@ -0,0 +1,165 @@
+#include "cartridge.h"
+#include "functions.h"
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <cerrno>
+#include <cassert>
+#include <cstring>
+#include <algorithm>
+
+Cartridge::Cartridge(std::string file_name) : 
+    mode(ROM), current_rom_bank(1), current_ram_bank(0), 
+    rom_bank_size(0x4000), // 16kB 
+    ram_bank_size(0x2000)  // 8kB 
+{
+    utils::load_file(cartridge_data, file_name);
+    cartridge_file_size = cartridge_data.size();
+    cartridge_type = get_type(cartridge_data[0x147]);
+    auto it = cartridge_data.begin();
+    game_title = std::string(it + 0x134, it + 0x143);
+
+    std::vector<CartridgeType> implemented = { ROM_ONLY, MBC1, MBC1_RAM, MBC1_RAM_BATTERY };
+    assert(std::find(implemented.begin(), implemented.end(), cartridge_type) != implemented.end());
+}
+
+u8 Cartridge::read(u16 addr) 
+{
+    switch (cartridge_type) 
+    {
+    case INVALID:
+    case ROM_ONLY:
+        return cartridge_data[addr];
+        break;
+
+    case MBC1:
+        if (addr < 0x4000) {
+            return cartridge_data[addr];
+        }
+        else if (addr >= 0x4000 && addr <= 0x7fff) {
+            u16 rom_bank_size = 0x4000; // 4kb
+            return cartridge_data[addr + ((current_rom_bank - 1) * rom_bank_size)];
+        }
+        else if (enable_ram && addr >= 0xa000 && addr <= 0xbfff) {
+            return cartridge_data[addr + current_ram_bank * ram_bank_size];
+        }
+        break;
+    }
+    assert(false);        
+}
+
+void Cartridge::write(u16 addr, u8 val)
+{
+    switch (cartridge_type)
+    {
+    case MBC1:
+        if (addr <= 0x1fff) { // enable/disable RAM
+            if (val == 0xa)
+                enable_ram = true;
+            else
+                enable_ram = false;
+        }
+        if (addr >= 0x2000 && addr <= 0x3fff) { // set 5lsb of ROM bank
+            current_rom_bank &= (7 << 5); 
+            current_rom_bank |= (0x1f & val); 
+            if (current_rom_bank & 0x1f == 0) 
+                current_rom_bank++;
+        }
+        else if (addr >= 0x4000 && addr <= 0x5fff) { // set RAM bank/2 msb of ROM bank 
+            if (mode == RAM) {
+                current_ram_bank = val & 0x3;
+            }
+            else if (mode == ROM) {
+                current_rom_bank &= (0x1f); 
+                current_rom_bank |= (val & 3) << 5; 
+                if (current_rom_bank & 0x1f == 0)
+                    current_rom_bank++;
+            }
+        }
+        else if (addr >= 0x6000 && addr <= 0x7fff) { // select mode
+            if (val == 0)
+                mode = ROM;
+            else
+                mode = RAM;
+        }
+        else if (enable_ram && addr >= 0xa000 && addr <= 0xbfff) { // access RAM
+            cartridge_data[addr + current_ram_bank * ram_bank_size] = val;
+        }
+        break;
+    }
+
+}
+
+std::string Cartridge::title() { return game_title; }
+
+std::string Cartridge::type() 
+{
+    switch (cartridge_type) {
+        case INVALID: return "INVALID";
+        case ROM_ONLY: return "ROM_ONLY";
+        case MBC1: return "MBC1";       
+        case MBC1_RAM: return "MBC1_RAM";       
+        case MBC1_RAM_BATTERY: return "MBC1_RAM_BATTERY";     
+        case MBC2:                            
+        case MBC2_BATTERY:                    
+        case ROM_RAM:                         
+        case ROM_RAM_BATTERY:                 
+        case MMM01:                           
+        case MMM01_RAM:                       
+        case MMM01_RAM_BATTERY:               
+        case MBC3_TIMER_BATTERY:              
+        case MBC3_RAM_TIMER_BATTERY:          
+        case MBC3:                            
+        case MBC3_RAM:                        
+        case MBC3_RAM_BATTERY:                
+        case MBC5:                            
+        case MBC5_RAM:                        
+        case MBC5_RAM_BATTERY:                
+        case MBC5_RUMBLE:                     
+        case MBC5_RUMBLE_RAM:                 
+        case MBC5_RUMBLE_RAM_BATTERY:         
+        case MBC6:                            
+        case MBC7_SENSOR_RUMBLE_RAM_BATTERY:  
+        case POCKET_CAMERA:                   
+        case BANDAI_TAMA5:                    
+        case HuC3:                            
+        case HuC1_RAM_BATTERY:
+        default:
+            return "UNIMPLEMENTED";
+    }
+}
+
+Cartridge::CartridgeType Cartridge::get_type(u8 val)
+{
+    switch (val) {
+        case 0x00: return Cartridge::ROM_ONLY;
+        case 0x01: return Cartridge::MBC1;
+        case 0x02: return Cartridge::MBC1_RAM;
+        case 0x03: return Cartridge::MBC1_RAM_BATTERY;
+        case 0x05: return Cartridge::MBC2;
+        case 0x06: return Cartridge::MBC2_BATTERY;
+        case 0x08: return Cartridge::ROM_RAM;
+        case 0x09: return Cartridge::ROM_RAM_BATTERY;
+        case 0x0b: return Cartridge::MMM01;
+        case 0x0c: return Cartridge::MMM01_RAM;
+        case 0x0d: return Cartridge::MMM01_RAM_BATTERY;
+        case 0x0f: return Cartridge::MBC3_TIMER_BATTERY;   
+        case 0x10: return Cartridge::MBC3_RAM_TIMER_BATTERY;
+        case 0x11: return Cartridge::MBC3;
+        case 0x12: return Cartridge::MBC3_RAM;
+        case 0x13: return Cartridge::MBC3_RAM_BATTERY;
+        case 0x19: return Cartridge::MBC5;
+        case 0x1a: return Cartridge::MBC5_RAM;
+        case 0x1b: return Cartridge::MBC5_RAM_BATTERY;
+        case 0x1c: return Cartridge::MBC5_RUMBLE;
+        case 0x1d: return Cartridge::MBC5_RUMBLE_RAM;
+        case 0x1e: return Cartridge::MBC5_RUMBLE_RAM_BATTERY;
+        case 0x20: return Cartridge::MBC6;
+        case 0x22: return Cartridge::MBC7_SENSOR_RUMBLE_RAM_BATTERY;
+        case 0xfc: return Cartridge::POCKET_CAMERA;
+        case 0xfd: return Cartridge::BANDAI_TAMA5;
+        case 0xfe: return Cartridge::HuC3;
+        case 0xff: return Cartridge::HuC1_RAM_BATTERY;
+        default: return Cartridge::INVALID;
+    }
+}
