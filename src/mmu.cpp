f051caf44ebde1392d1d1327f2c43fc3fb57aa20
rmaccrimmon <r.maccrimm@gmail.com>
2018-11-13 15:07:15 -0700

    Reorganize memory read/write, add functions for handling special registers

--- src/mmu.cpp
+++ src/mmu.cpp
@@ -1,8 +1,13 @@
 #include "mmu.h"
 #include "functions.h"
 #include "registers.h"
+#include "register8bit.h"
 #include <cstring>
 #include <iostream>
+#include <cassert>
+#include <algorithm>
+
+typedef Register8bit r8;
 
 Memory::Memory(Cartridge *cart, Joypad *pad, bool enable_boot) : 
     cartridge(cart), 
@@ -12,7 +17,62 @@ Memory::Memory(Cartridge *cart, Joypad *pad, bool enable_boot) :
     enable_break_pt(false), 
     paused(false), 
     vram_updated(false) 
-{}
+{
+    mem.reserve(0x10000);
+    init_registers();
+}
+
+u8 Memory::read(u16 addr) 
+{
+    switch(addr / 0x1000) 
+    {
+    // Switchable ROM Bank
+    case 0x0: 
+        if (addr <= 0x100 && (!(mem[0xff50] & 1) && enable_boot_rom)) { 
+            return boot_rom[addr];
+        } 
+    case 0x1:
+    case 0x2:
+    case 0x3:
+    case 0x4:
+    case 0x5:
+    case 0x6:
+    case 0x7: 
+        return cartridge->read(addr);
+    // VRAM        
+    case 8: 
+    case 9:
+        return mem[addr];
+    // Switchable RAM bank
+    case 0xa: 
+    case 0xb:
+        return cartridge->read(addr);
+    // Internal RAM
+    case 0xc:
+    case 0xd:
+        return mem[addr];
+    case 0xe:
+    case 0xf:
+        if (special_registers.find(addr) != special_registers.end()) {
+            return read_reg(addr);
+        }
+        else if (addr <= 0xfdff) { // Echo RAM
+            return mem[addr - 0x2000];
+        }
+        else if (addr <= 0xfe9f) { // OAM
+            return mem[addr];
+        }
+        else if (addr <= 0xfeff) { // unusable memory
+            return 0xff;
+        }
+        else if (addr >= 0xff00) { // I/O Registers + High RAM
+            return mem[addr];
+        }   
+    default:
+        std::cout << "Read from address " << addr << std::endl;
+        assert(false);
+    }
+}
 
 void Memory::write(u16 addr, u8 data)
 {
@@ -20,60 +80,80 @@ void Memory::write(u16 addr, u8 data)
         paused = true; 
     }
 
-    if (addr <= 0x7fff) { // select RAM/ROM bank
+    switch(addr / 0x1000) {
+    // Select RAM/ROM bank                 
+    case 0x0: 
+    case 0x1:
+    case 0x2:
+    case 0x3:
+    case 0x4:
+    case 0x5:
+    case 0x6:
+    case 0x7: 
         cartridge->write(addr, data);
-        return;
-    }
-    else if (addr >= 0x8000 && addr <= 0x9fff) { // VRAM
-		vram_updated = true;
-	}
-    else if (addr >= 0xa000 && addr <= 0xbfff) { // Switchable RAM bank
+        break;
+    // VRAM        
+    case 8: 
+    case 9:
+        vram_updated = true;
+        mem[addr] = data;
+        break;
+    // Switchable RAM bank
+    case 0xa: 
+    case 0xb:
         cartridge->write(addr, data);
-        return;
+        break;
+    // Internal RAM
+    case 0xc:
+    case 0xd:
+        mem[addr] = data;
+        break;
+    case 0xe:
+    case 0xf:
+        if (special_registers.find(addr) != special_registers.end()) {
+            write_reg(addr, data);
+        }
+        else if (addr <= 0xfdff) { // Echo RAM
+            mem[addr - 0x2000] = data;
+        }
+        else if (addr <= 0xfe9f) { // OAM
+            vram_updated = true;
+            mem[addr] = data;
+        }
+        // 0xfea0-0xfeff not usable
+        else if (addr >= 0xff00 ) {
+            mem[addr] = data;
+        }
+        break;
+    default:
+        std::cout << "Wrote to address " << addr << std::endl;
+        assert(false);
     }
-    else if (addr >= 0xe000 && addr < 0xfe00) { // Echo RAM
-        addr -= 0x2000;
-    }
-    else if (addr >= 0xfea0 && addr <= 0xfeff) { // unusable memory
-        return;
-    }
-    else if (addr == reg::DIV) {
-        // writing any value to DIV writes 0 and resets system counter
-        /*mem[addr] = 0;
-        clock_counter->set(0);*/
-    }
-    mem[addr] = data;
 }
 
-u8 Memory::read(u16 addr) 
+u8 Memory::read_reg(u16 addr) 
 {
-    if (addr <= 0x100 && (!(mem[0xff50] & 1) && enable_boot_rom)) { 
-            return boot_rom[addr];
-    } 
-
-    if (addr <= 0x7fff) { // Switchable ROM bank
-        return cartridge->read(addr);
-    }
-    else if (addr >= 0xa000 && addr <= 0xbfff) { // Switchable RAM bank
-        return cartridge->read(addr);
-    }
-
-    if (addr >= 0xe000 && addr < 0xfdff) { // Echo RAM
-        return mem[addr - 0x2000];
-    }
-    else if (addr >= 0xfea0 && addr <= 0xfeff) { // unusable memory
-        return 0xff;
-    }
-    else if (addr == reg::IF) {
-
-    }
-    else if (addr == 0xff00) {
-        // if bit 4 is reset select dpad, assume dpad/buttons mutually exclusive
+    switch(addr)
+    {
+    case reg::JOYP:
+    {
         bool select_dpad = (mem[addr] & (1 << 4)) == 0;
         return joypad->get_state(select_dpad);
     }
+    default:
+        return mem[addr];
+    }
+}
 
-    return mem[addr];
+void Memory::write_reg(u16 addr, u8 data)
+{
+    switch(addr)
+    {
+    case reg::DIV:
+        mem[addr] = 0;
+    default:
+        mem[addr] = data;
+    }
 }
 
 void Memory::set_flags(u16 addr, u8 mask, bool b)
@@ -90,11 +170,6 @@ void Memory::load_boot(std::string file_path)
     utils::load_file(boot_rom, file_path);
 }
 
-void Memory::load(u8 data[], size_t offset, size_t size)
-{
-    memcpy(mem + offset, data, size);
-}
-
 void Memory::set_access_break_pt(u16 addr) 
 { 
     enable_break_pt = true;
@@ -105,4 +180,10 @@ void Memory::clear_access_break_pt() { enable_break_pt = false; }
 
 bool Memory::pause() { return paused; }
 
-u8* Memory::get_mem_ptr(u16 addr) { return &mem[addr]; }
+u8* Memory::get_mem_ptr(u16 addr) { return &mem.data()[addr]; }
+
+void Memory::init_registers()
+{
+    special_registers.insert(reg::DIV);
+    special_registers.insert(reg::JOYP);
+}
