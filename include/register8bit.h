bddf24d622c951b098e070ac52b19b01323d19f3
Roddy MacCrimmon <r.maccrimm@gmail.com>
2018-11-14 10:10:01 -0700

    Add unused and read-only options for bits

--- include/register8bit.h
+++ include/register8bit.h
@@ -9,8 +9,11 @@ public:
     // Read/Write register initialized to 0
     Register8bit();
 
-    // Mask out read-only bits
-    Register8bit(u8 w_mask);
+    // Unused bits will always store 1
+    Register8bit(u8 unused);
+
+    // Read-only bits will be modified by set but not write
+    Register8bit(u8 unused, u8 read_only);
     
     // Directly set contents of register
     void set(u8 x);
@@ -18,6 +21,7 @@ public:
     // Set contents, masking out read-only bits
     void write(u8 x);
 
+    // Get contents of register
     u8 value() const;
     
     void increment();
@@ -31,6 +35,7 @@ public:
 private:
     u8 byte;
     u8 write_mask;
+    u8 read_mask;
 };
 
 #endif
--- src/register8bit.cpp
+++ src/register8bit.cpp
@@ -1,12 +1,25 @@
 #include "register8bit.h"
 
-Register8bit::Register8bit(): byte(0), write_mask(0xff) {}
+Register8bit::Register8bit(): byte(0), read_mask(0), write_mask(0) {}
 
-Register8bit::Register8bit(u8 w_mask): byte(0), write_mask(w_mask) {}
+Register8bit::Register8bit(u8 unused): byte(0), read_mask(unused), write_mask(0) {}
 
-u8 Register8bit::value() const { return byte; }
+Register8bit::Register8bit(u8 unused, u8 read_only): 
+    byte(0), read_mask(unused), write_mask(read_only) {}
 
 void Register8bit::set(u8 x) { byte = x; }
+
+void Register8bit::write(u8 x) 
+{ 
+    // bits which are set in write_mask are ignored in x
+    byte = (x & ~write_mask) | (byte & write_mask); 
+}
+
+u8 Register8bit::value() const 
+{ 
+    // bits in read_mask are always 1
+    return byte | read_mask; 
+}
 
 void Register8bit::increment() { byte++; }
 
@@ -16,7 +29,4 @@ void Register8bit::add(u8 x) { byte += x; }
 
 void Register8bit::sub(u8 x) { byte -= x; }
 
-void Register8bit::write(u8 x) 
-{ 
-    byte = (x & write_mask) | (byte & ~write_mask); 
-}
+
