720e81c590f105dd412451c47c65928a3610c9f9
Roddy MacCrimmon <r.maccrimm@gmail.com>
2018-11-23 20:28:54 -0700

    simplify shaders, just draw single texture to quad

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -1,23 +1,9 @@
 #version 330 core
 out vec4 frag_color;
-in vec2 bg_tex_coords;
-in vec2 sprite_tex_coords;
-uniform usampler2D background_texture;
-uniform usampler2D sprite_texture;
-uniform usampler2D sprite_priority;
-uniform usampler2D window_texture;
+in vec2 tex_coords;
+uniform usampler2D screen_texture;
 void main() {
-    float col;
-    uint bg_val = texture(background_texture, bg_tex_coords).r;
-    uint sprite_val = texture(sprite_texture, sprite_tex_coords).r;
-    
-    uint val;
-    if (texture(sprite_texture, sprite_tex_coords).g != 0U) {
-        val = sprite_val;
-    }
-    else {
-        val = bg_val;
-    }
-    col = float(val) / 255.0;
+    uint val = texture(background_texture, bg_tex_coords).r;
+    float col = float(bg_val) / 255.0;
     frag_color = vec4(col, col, col, 1);
 }
--- src/vertex.glsl
+++ src/vertex.glsl
@@ -1,13 +1,8 @@
 #version 330 core
 layout (location = 0) in vec3 inPos;
 layout (location = 1) in vec2 inTexCoords;
-out vec2 bg_tex_coords;
-out vec2 sprite_tex_coords;
-uniform int scrollx;
-uniform int scrolly;
+out vec2 tex_coords;
 void main() {
     gl_Position = vec4(inPos, 1.0);
-    // Top of screen at pixel 144, top of background at 256 so first shift up by 112
-    bg_tex_coords = vec2(inTexCoords.x + scrollx, inTexCoords.y + 112.0 - scrolly) / 256.0;
-    sprite_tex_coords = vec2(inTexCoords.x + 8, inTexCoords.y + 8) / 176;
+    tex_coords = vec2(inTexCoords.x / 160.0, inTexCoords.y / 144.0);
 }
